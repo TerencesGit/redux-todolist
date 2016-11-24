acc1507798e2adef33ea4a05da1b534588cbd28f
transformGit <2630243397@qq.com>
2016-11-24 21:13:48 +0800

    entry point

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack')
+module.exports = {
+	entry: [
+				'webpack/hot/dev-server',
+				'webpack-dev-server/client?http://localhost:8080',
+				'./src/index',
+	],
+	output: {
+		filename: 'bundle.js',
+		publicPath: '/static/'
+	},
+	resolve: {
+		extensions: ['', '.js', 'jsx']
+	},
+	module: {
+		loaders: [{
+			text: /\.jsx?$/,
+			exclude: /node_modules/,
+			loaders: ['babel?presets[]=es2015&presets[]=react']
+		}]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	]
+}
